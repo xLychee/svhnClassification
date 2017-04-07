89ed5feeb1964f56d8ef94aaae6577bc49505e19
xLychee <xun.luan.rice@gmail.com>
2017-04-07 12:37:09 -0500

    dd

--- dataprocess3.py
+++ dataprocess3.py
@@ -16,8 +16,8 @@ y1 = np.load('../train_y.npy')
 X2 = np.load('../extra_X.npy')
 y2 = np.load('../extra_y.npy')
 
-X3 = np.vstack(X1,X2)
-y3 = np.vstack(y1,y2)
+X3 = np.vstack((X1,X2))
+y3 = np.vstack((y1,y2))
 
-X3.save('new_X.npy')
-y3.save('new_y.npy')
+X3.save('../new_X.npy')
+y3.save('../new_y.npy')
