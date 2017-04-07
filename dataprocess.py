6baa4e19d947a5f1cee0d450be3ff5de71925ddb
xLychee <xun.luan.rice@gmail.com>
2017-04-06 21:43:09 -0500

    dd

--- dataprocess.py
+++ dataprocess.py
@@ -15,9 +15,16 @@ X = np.array(df.iloc[:,1:])
 X = X.reshape(32, 32, 3, -1).transpose(3,0,1,2)
 y = np.array(df.iloc[:,0])
 
-import matplotlib.pyplot as plt
-plt.imshow(X[2])
-
+#import matplotlib.pyplot as plt
+#plt.imshow(X[2])
+np.save('../train_X',X)
 np.save('../train_y',y)
 
-b = np.load('../train_y.npy')
+#b = np.load('../train_y.npy')
+
+
+df2 = pd.read_csv('../test.csv')
+X = np.array(df2)
+X = X.reshape(32, 32, 3, -1).transpose(3,0,1,2)
+np.save('../test_X',X)
+
